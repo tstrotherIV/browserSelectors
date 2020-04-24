e295ce0b58c7ae0046873adb4559d9d298064403
Tom Strother <tom_strother@yahoo.com>
2020-04-24 11:02:01 -0500

    edited

--- eventHandlers.js
+++ eventHandlers.js
@@ -1,11 +1,19 @@
-document.getElementById("message").addEventListener("keyup", () => {
-  document.getElementById("item1").innerHTML = event.target.value;
-  document.getElementById("item2").innerHTML = event.target.value;
-});
+// document.getElementById("message").addEventListener("keyup", () => {
+//   document.getElementsByClassName("item1").innerHTML = event.target.value;
+//   document.getElementsByClassName("item2").innerHTML = event.target.value;
+// });
 
+// document.querySelector("message").addEventListener("keyup", function(event) => {}
 // let messageSection = document.getElementById("message");
-// let outputSection = document.querySelectorAll("#item1");
+// let outputSection = document.querySelectorAll("item1");
 
 // messageSection.addEventListener("keyup", function() {
 //   outputSection.innerHTML = event.target.value;
 // });
+
+document.querySelector("#message").addEventListener("keyup", function(event) {
+  let boxes = document.querySelectorAll(".article-box");
+  for (const box of boxes) {
+    box.innerHTML = event.target.value;
+  }
+});
